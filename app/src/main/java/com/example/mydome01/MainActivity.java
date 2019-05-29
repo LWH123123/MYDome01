245488f3078ea0430e421f391a1218230152a116
lwh123456 <2365806605@qq.com>
2019-05-29 17:15:05 +0800

    打印了一行log日志

--- app/src/main/java/com/example/mydome01/MainActivity.java
+++ app/src/main/java/com/example/mydome01/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mydome01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Log.i(TAG, "onCreate: 第一次使用git");
+
     }
+
+    private static final String TAG = "MainActivity";
+
 }
